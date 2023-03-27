4ab9cfa152092c85f5acf02f38258bf67709f7d4
Filip Bořil <borilfilip@gmail.com>
2023-03-27 17:29:19 +0200

    Displaying data

--- src/Converter.tsx
+++ src/Converter.tsx
@@ -3,6 +3,14 @@ import { useQuery } from "@tanstack/react-query";
 import { ReactElement } from "react";
 import { apiUrl } from "./App";
 
+interface Currency {
+  country: string;
+  currency: string;
+  amount: string;
+  code: string;
+  rate: string;
+}
+
 function Converter(): ReactElement {
   const { isLoading, error, data, isFetching } = useQuery({
     queryKey: ["repoData"],
@@ -19,8 +27,50 @@ function Converter(): ReactElement {
       </div>
     );
 
+  let currencies: { [code: string]: Currency } = {};
+  if (data) {
+    const rows = data.split("\n");
+    rows.forEach((entry: string, line: number) => {
+      if (line > 1 && entry) {
+        const cols = entry.split("|");
+        currencies = {
+          ...currencies,
+          [cols[3]]: {
+            country: cols[0],
+            currency: cols[1],
+            amount: cols[2],
+            code: cols[3],
+            rate: cols[4],
+          },
+        };
+      }
+    });
+  }
+
   return (
     <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Country</th>
+            <th>Currency</th>
+            <th>Amount</th>
+            <th>Code</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.values(currencies).map((currency: Currency) => (
+            <tr key={currency.code}>
+              <td>{currency.country}</td>
+              <td>{currency.currency}</td>
+              <td>{currency.amount}</td>
+              <td>{currency.code}</td>
+              <td>{currency.rate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <div>{isFetching ? "Updating..." : ""}</div>
     </div>
   );
